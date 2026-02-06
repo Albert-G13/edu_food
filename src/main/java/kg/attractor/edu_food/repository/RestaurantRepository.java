263de4c429fa484f4e4d54ce84d86181c046cbda
Albert G. <a.golovanov1323@gmail.com>
2026-02-06 13:14:58 +0600

    Created entities, repositories, and DTOs

---
+++ src/main/java/kg/attractor/edu_food/repository/RestaurantRepository.java
@@ -0,0 +1,14 @@
+package kg.attractor.edu_food.repository;
+
+import kg.attractor.edu_food.model.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    Page<Restaurant> findByNameContainingIgnoreCase(String name, Pageable page);
+}
